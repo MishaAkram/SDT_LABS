b4f57051e409aae7be39ea41938cbdf0ebb16243
Iqra-Irfan <74137053+Iqra-Irfan@users.noreply.github.com>
2021-07-18 17:59:23 +0500

    Lab 5 question 3 added

---
+++ src/lab5_q3/Broker.java
@@ -0,0 +1,20 @@
+package lab5_q3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Broker {
+	private List<Order> orderList = new ArrayList<Order>(); 
+
+	   public void takeOrder(Order order){
+	      orderList.add(order);		
+	   }
+
+	   public void placeOrders(){
+	   
+	      for (Order order : orderList) {
+	         order.execute();
+	      }
+	      orderList.clear();
+	   }
+}
---
+++ src/lab5_q3/SellStock.java
@@ -0,0 +1,13 @@
+package lab5_q3;
+
+public class SellStock implements Order {
+	   private Stock abcStock;
+
+	   public SellStock(Stock abcStock){
+	      this.abcStock = abcStock;
+	   }
+
+	   public void execute() {
+	      abcStock.sell();
+	   }
+	}
